e31e423a5b52fd51b4ca724abb0831b9679db99a
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-25 22:27:22 +0200

    add basic test for this plugin
    Fixes #13

--- spec/inputs/irc_spec.rb
+++ spec/inputs/irc_spec.rb
@@ -1,3 +1,18 @@
 # encoding: utf-8
-require "logstash/devutils/rspec/spec_helper"
-require 'logstash/inputs/irc'
+require_relative "../spec_helper"
+require "logstash/plugin"
+require "logstash/event"
+
+describe LogStash::Inputs::Irc do
+
+  let(:properties) { {:name => "foo" } }
+  let(:event)      { LogStash::Event.new(properties) }
+
+  let(:host)       { "freenode.org" }
+  let(:channels)   { ["foo", "bar"] }
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("input", "irc").new({ "host" => host, "channels" => channels })
+    expect { plugin.register }.to_not raise_error
+  end
+end
