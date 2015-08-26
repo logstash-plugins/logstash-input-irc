c36fe6c22feacd509278b665da84d7131fd7cf23
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-26 13:47:34 +0200

    Add a simple test to the project and a few refactoring to include a nice shutdown procedure, plus the option to inject a dummy bot (useful for testing)
    Fixes #13

--- spec/inputs/irc_spec.rb
+++ spec/inputs/irc_spec.rb
@@ -2,17 +2,71 @@
 require_relative "../spec_helper"
 require "logstash/plugin"
 require "logstash/event"
+require "cinch"
 
 describe LogStash::Inputs::Irc do
 
   let(:properties) { {:name => "foo" } }
   let(:event)      { LogStash::Event.new(properties) }
 
-  let(:host)       { "freenode.org" }
+  let(:host)       { "irc.freenode.org" }
   let(:channels)   { ["foo", "bar"] }
 
   it "should register without errors" do
     plugin = LogStash::Plugin.lookup("input", "irc").new({ "host" => host, "channels" => channels })
     expect { plugin.register }.to_not raise_error
+  end
+
+  describe "receive" do
+
+    let(:config) { { "host" => host, "channels" => channels } }
+    subject      { LogStash::Inputs::Irc.new(config) }
+
+    let(:msg)    { double("message") }
+    let(:user)   { double("user") }
+
+    before(:each) do
+      allow(msg).to receive(:message).and_return("message")
+      allow(msg).to receive(:command).and_return("command")
+      allow(user).to receive(:host).and_return("host")
+      allow(user).to receive(:nick).and_return("nick")
+      allow(msg).to receive(:user).and_return(user)
+      allow(msg).to receive(:prefix).and_return("prefix")
+      allow(msg).to receive(:channel).and_return("channel")
+
+      subject.inject_bot(bot)
+      subject.register
+    end
+
+    let(:bot)     { Cinch::Bot.new }
+    let(:channel) { bot.handlers.find(:channel).first }
+    let(:nevents) { 1 }
+
+    let(:events) do
+      input(subject) do |queue|
+        nevents.times do
+          channel.call(msg, [], [])
+        end
+        result = nevents.times.inject([]) do |acc|
+          acc << queue.pop
+        end
+        result
+      end
+    end
+
+    let(:event) { events.first }
+
+    it "receive events from a channel" do
+      expect(event["channel"]).to eq("channel")
+    end
+
+    it "receive events with a command" do
+      expect(event["command"]).to eq("command")
+    end
+
+    it "receive events with nick information" do
+      expect(event["nick"]).to eq("nick")
+    end
+
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,24 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require 'logstash/inputs/irc'
+
+module IrcHelpers
+
+  def input(plugin, &block)
+    queue = Queue.new
+
+    input_thread = Thread.new do
+      plugin.run(queue)
+    end
+    result = block.call(queue)
+
+    plugin.teardown
+    input_thread.join
+    result
+  end
+
+end
+
+RSpec.configure do |c|
+  c.include IrcHelpers
+end
