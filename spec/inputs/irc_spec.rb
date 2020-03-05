2bcad798badc9dadc319a94304193931dded3cba
Karol Bucek <self@kares.org>
2020-03-05 20:46:00 +0100

    Test: compatibility with devutils 2.0

--- spec/inputs/irc_spec.rb
+++ spec/inputs/irc_spec.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require_relative "../spec_helper"
+require "logstash/devutils/rspec/shared_examples"
 require "logstash/plugin"
 require "logstash/event"
 require "cinch"
@@ -90,4 +91,21 @@ describe LogStash::Inputs::Irc do
       end
     end
   end
+
+  private
+
+  # reinvented - has been deprecated in devutils 2.0
+  def plugin_input(plugin, &block)
+    queue = Queue.new
+
+    input_thread = Thread.new do
+      plugin.run(queue)
+    end
+    result = block.call(queue)
+
+    plugin.do_stop
+    input_thread.join
+    result
+  end
+
 end
